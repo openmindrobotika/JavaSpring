e9281d3d059e265c1083166fe1d4a80d80aa1102
ALUMNO <ALUMNO@PC412.mshome.net>
2022-05-12 14:18:57 +0200

    12

---
+++ TareaF19/src/tareaf19/Main.java
@@ -0,0 +1,10 @@
+package tareaf19;
+import comprobaciones.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Comprobaciones.JOpaneIntCancel("pepe", "introduce numero", 0);
+	}
+
+}
---
+++ TareaF19/src/tareaf19/TareaF19_2.java
@@ -0,0 +1,5 @@
+package tareaf19;
+
+public class TareaF19_2 {
+	private 
+}
